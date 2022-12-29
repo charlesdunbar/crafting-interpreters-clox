f8da6663d1ed93ff2c4165fb7fdfabd5616bae67
Charles Dunbar <ccdunbar@gmail.com>
2022-12-29 13:50:55 -0600

    Finish chapter 14 - working chunks, constants, and line numbers

--- chunk.c
+++ chunk.c
@@ -11,23 +11,35 @@ void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
+    initValueArray(&chunk->constants);
 }
 
 /// @brief Write a byte to the chunk array, resizing if we're at capcity
 /// @param chunk - chunk to append to
 /// @param byte - byte to append to chunk
-void writeChunk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
+}
+
+int addConstant(Chunk* chunk, Value value) {
+    writeValueArray(&chunk->constants, value);
+    // Return the index where the constant was appended so we can located it later
+    return chunk->constants.count - 1;
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
