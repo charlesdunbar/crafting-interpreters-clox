72bcc0857077a0c216220a5ea88593bdf910c21a
Charles Dunbar <ccdunbar@gmail.com>
2023-02-08 22:41:44 -0600

    Up to 25.3 - creating upvalue objects

--- compiler.c
+++ compiler.c
@@ -45,6 +45,11 @@ typedef struct {
     int depth; //< How deep in scope the variable is. 0 is global, 1 is first block, etc.
 } Local;
 
+typedef struct {
+    uint8_t index;
+    bool isLocal;
+} Upvalue;
+
 /**
  * @brief Used for determining top-level code vs body of function code
  */
@@ -60,6 +65,7 @@ typedef struct Compiler {
 
     Local locals[UINT8_COUNT];
     int localCount; //< How many locals are in scope.
+    Upvalue upvalues[UINT8_COUNT];
     int scopeDepth; //< Number of blocks surrounding the current part of code we're compiling.
 } Compiler;
 
@@ -311,6 +317,51 @@ static int resolveLocal(Compiler* compiler, Token* name) {
     return -1; // Assume global variable
 }
 
+static int addUpvalue(Compiler* compiler, uint8_t index, bool isLocal) {
+    int upvalueCount = compiler->function->upvalueCount;
+
+    // If we already have the upvalue, return that early.
+    for (int i = 0; i < upvalueCount; i++) {
+        Upvalue* upvalue = &compiler->upvalues[i];
+        if (upvalue->index == index && upvalue->isLocal == isLocal) {
+            return i;
+        }
+    }
+
+    if (upvalueCount == UINT8_COUNT) {
+        error("Too many closure variables in function.");
+        return 0;
+    }
+
+    compiler->upvalues[upvalueCount].isLocal = isLocal;
+    compiler->upvalues[upvalueCount].index = index;
+    return compiler->function->upvalueCount++;
+}
+
+/**
+ * @brief Look at the enclosing function to see if we can find a variable decalred there
+ * @param compiler 
+ * @param name 
+ * @return 
+ */
+static int resolveUpvalue(Compiler* compiler, Token* name) {
+    if (compiler->enclosing == NULL) return -1;
+
+    // If we have a local variable in the enclosing function, capture it.
+    int local = resolveLocal(compiler->enclosing, name);
+    if (local != -1) {
+        return addUpvalue(compiler, (uint8_t)local, true);
+    }
+
+    // Otherwise, capture the upvalue of the enclosing function.
+    int upvalue = resolveUpvalue(compiler->enclosing, name);
+    if (upvalue != -1){
+        return addUpvalue(compiler, (uint8_t)upvalue, false);
+    }
+
+    return -1;
+}
+
 /**
  * @brief Add a local variable to the compiler's 'locals' array.
  * @param name Name of variable to store.
@@ -487,6 +538,9 @@ static void namedVariable(Token name, bool canAssign) {
     if (arg != -1) {
         getOp = OP_GET_LOCAL;
         setOp = OP_SET_LOCAL;
+    } else if ((arg = resolveUpvalue(current, &name)) != -1 ) {
+        getOp = OP_GET_UPVALUE;
+        setOp = OP_SET_UPVALUE;
     } else {
         arg = identifierConstant(&name);
         getOp = OP_GET_GLOBAL;
@@ -736,6 +790,13 @@ static void function(FunctionType type) {
 
     ObjFunction* function = endCompiler();
     emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
+
+    // OP_CLOSURE's first operand is 1 if the variable is local, 0 for an upvalue
+    // the second operand is the index of the variable/upvalue.
+    for (int i = 0; i < function->upvalueCount; i++) {
+        emitByte(compiler.upvalues[i].isLocal ? 1 : 0);
+        emitByte(compiler.upvalues[i].index);
+    }
 
 }
 
