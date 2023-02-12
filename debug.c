d7ef32b733f567aa1eb98ce818c64c803ba32683
Charles Dunbar <ccdunbar@gmail.com>
2023-02-12 10:38:47 -0600

    Cleanup

--- debug.c
+++ debug.c
@@ -45,7 +45,7 @@ static int simpleInstruction(const char* name, int offset) {
 
 static int byteInstruction(const char* name, Chunk* chunk, int offset) {
     uint8_t slot = chunk->code[offset + 1];
-    printf("%-16s %d\n", name, slot);
+    printf("%-16s %4d\n", name, slot);
     return offset + 2;
 }
 
